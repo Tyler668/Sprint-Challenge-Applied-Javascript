ec2be17ff87978702125e01d0b8638326f8b7ac3
Tyler Gilchrist <tyler668@gmail.com>
2019-08-09 12:33:04 -0400

    articles done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,9 @@
+import { create } from "domain";
+
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +19,71 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function createCard(cardObject) {
+    // Create elements ===
+    const baseCard = document.createElement('div');
+    const headline = document.createElement('div');
+    const authorInfo = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+
+    // Structuring elements === 
+    baseCard.appendChild(headline);
+    baseCard.appendChild(authorInfo);
+    authorInfo.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    authorInfo.appendChild(authorName);
+
+
+    // Applying classes to elements ===
+    baseCard.classList.add('card');
+    headline.classList.add('headline');
+    authorInfo.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+
+    // Filling element content ===
+    headline.textContent = cardObject.headline;
+    img.src = cardObject.authorPhoto;
+    authorName.textContent = `By ${cardObject.authorName}`;
+
+
+    return baseCard;
+
+}
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        //Was breaking unless I did these individually and 
+        // I'm on a time constraint so blegh 
+
+        response.data.articles.javascript.forEach(element=>{
+            cardsContainer.appendChild(createCard(element));
+        });
+
+        response.data.articles.bootstrap.forEach(element=>{
+            cardsContainer.appendChild(createCard(element));
+        });
+
+        response.data.articles.jquery.forEach(element=>{
+            cardsContainer.appendChild(createCard(element));
+        });
+
+        response.data.articles.node.forEach(element=>{
+            cardsContainer.appendChild(createCard(element));
+        });
+
+        response.data.articles.technology.forEach(element=>{
+            cardsContainer.appendChild(createCard(element));
+        });
+    });
+
+
+//REMEMBER TO UNCOMMENT TOP THING
